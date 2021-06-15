918679bc779590fa7ec53e1ed7bbae3ab0af6bbb
gsbakshi <greaser.bakshi@gmail.com>
2021-06-15 14:33:44 +0530

    4 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import HomePage from './pages/homepage/homepage.component.jsx';
+
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
-}
+const App = () => {
+  return <HomePage />;
+};
+
+export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,48 @@
-import React form 'react';
+import React, { useState, useEffect } from 'react';
 
-const Directory = () => ();
+import './directory.styles'
+
+const Directory = () => {
+  const [sections, setSections] = useState([
+    {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+          linkUrl: 'hats'
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+          linkUrl: ''
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+          linkUrl: ''
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+          linkUrl: ''
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+          linkUrl: ''
+        }
+  ]);
+
+  return (
+    <div className = 'directory-menu'> 
+    {sections.map(({id, ...otherProps}) => (<h1>{...otherProps.title}</h1>))}
+    </div>
+  );
+};
 
 export default Directory;
