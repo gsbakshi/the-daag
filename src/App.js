897d24b08310073d2c36726ac52707c089804d85
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-06-15 22:01:49 +0530

    Completed homepage view. Using hooks instead of class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
 import './App.css';
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        {/* <Route path='/shop' component={ ShopPage } />
+        <Route path='/signin' component={ SignInAndSignUpPage } /> */}
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import MenuItem from '../menu-item/menu-item.component.jsx';
 
 import './directory.styles.scss'
 
 const Directory = () => {
-  const [sections, setSections] = useState([
+  const [sections] = useState([
     {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
@@ -16,27 +16,27 @@ const Directory = () => {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: ''
+          linkUrl: 'jackets'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: ''
+          linkUrl: 'sneakers'
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: ''
+          linkUrl: 'womens'
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: ''
+          linkUrl: 'mens'
         }
   ]);
 
