09274bd484400297456323b38578391107b290cf
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-06-21 05:13:48 +0530

    Added Redux into our application. Setting up the boilerplate code

---
+++ src/redux/shop/shop.reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+    currentUser: null
+};
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default shopReducer;
